17f646ed0b2c0c94f0a9b75058eddd1a41bf6b91
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-10-28 15:12:44 -0400

    ...

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const routes = require("./routes");
 
 const app = express();
 
+require('dotenv').config();
+
 app.use(routes)
 
 
 app.listen(process.env.PORT, () => {
-    console.log("Listening on port 3000 ............");
+    console.log("Listening on port 3001 ............");
 })
