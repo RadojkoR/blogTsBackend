cee71b3608f98cf8946c66c66f395465232963e4
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-10-29 15:10:21 -0400

    ...

--- controllers/CountriesController.js
+++ controllers/CountriesController.js
@@ -1,7 +1,17 @@
+const { json } = require('express');
 const db = require('../database/config');
 
 const getCountriesByContinent = async(res,req) => {
-    console.log("Request parameters:", req.params);
+    const{continent} = req.params;
+    try {
+        const [countries] = await db.query("SELECT * FROM country WHERE continent = ?", [continent]);
+        res,json(countries)
+    }catch(error){
+        console.error("Error Fetching Countries:", error);
+        res.status(500).json({message: "Database connection error"})
+        
+    }
+    
 }
 
 module.exports = {
