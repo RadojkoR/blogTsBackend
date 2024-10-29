00959e3ac7c8a61dc1952e68ef7e0987f0371468
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-10-29 12:02:56 -0400

    ...

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const routes = require("./routes");
+
 const PORT = process.env.PORT;
 require('dotenv').config();
 
 const app = express();
 
 app.use(routes)
-
 
 app.listen(PORT,'0.0.0.0', () => {
     console.log("Listening on port 3001 ............");
