ad54702349f3db5d84dbd8a5721718cb0f1c04a4
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-10-30 13:53:09 -0400

    added BlogController

---
+++ controllers/BlogController.js
@@ -0,0 +1,19 @@
+const { json } = require('express');
+const db = require('../database/config');
+
+const index = async(req,res) => {
+    try {
+         const [blogData] = await db.query("select * from blogs");
+        res.json(countryData)
+        console.log("blogs Data", blogData);
+        
+    }catch(error){
+        console.error("Error Fetching Countries:", error);
+        res.status(500).json({message: "Database connection error"})
+        
+    }
+}
+
+module.exports = {
+    index
+}
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const HomeController = require("../controllers/HomeController");
 const CountriesController = require("../controllers/CountriesController");
+const BlogController = require("../controllers/BlogController")
 
 const router = express();
 
@@ -9,5 +10,6 @@ router.use(cors());
 
 router.get("/",HomeController.index);
 router.get("/destination", CountriesController.getCountriesByContinent);
+router.get("/blogs", BlogController.index);
 
 module.exports = router
