c56e438a986b084911fc1905de1ce674247df8fe
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-11-03 11:05:30 -0500

    ...

--- controllers/ContinentsController.js
+++ controllers/ContinentsController.js
@@ -1,15 +1,13 @@
 const db = require('../database/config');
 
 const index = async(req,res) => {
-    try{
-        const [continentsData] = await db.query("select * from continents");
-        console.log('Continents data', continentsData);
+    try {
+        const [posts] = await db.query("select * from blog_posts");
+        res.json(posts);
         
-        res.json(continentsData);
-
     }catch(error){
-        console.error("Error Fetching Continents", error);
-        res.status(500).json({message: "Database conection error"})
+        console.error("Error Fetching Countries:", error);
+        res.status(500).json({message: "Database connection error"})
         
     }
 }
