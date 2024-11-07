9fcbde8a010180de88d0d71746bbc86df3ce9425
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-11-06 19:19:37 -0500

    ....

--- controllers/ContinentsController.js
+++ controllers/ContinentsController.js
@@ -1,7 +1,6 @@
 const db = require('../database/config');
 const multer = require('multer')
 const path = require('path');
-const upload = multer({ dest: 'uploads/' });
 
 const index = async(req,res) => {
     try{
@@ -18,16 +17,7 @@ const index = async(req,res) => {
 // Create New Continent
 const create = async(req, res) => {
     const { continent_name, continent_img} = req.body;
-    const imgFile = req.imgFile;
-    console.log('Received data:',continent_name, imgFile); // Ovo će prikazati podatke koji dolaze u POST zahtevu
-
-     if (imgFile) {
-    // Dodatne operacije sa fajlom
-    res.status(200).json({ message: 'Continent added successfully!' });
-  } else {
-    res.status(400).json({ message: 'Image file missing' });
-  }
-
+    console.log('Received data:', req.file); // Ovo će prikazati podatke koji dolaze u POST zahtevu
     if (!continent_name || !continent_img) {
         return res.status(400).json({ message: "Continent name is required." });
     }
