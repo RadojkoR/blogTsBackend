b3371002f7042e13e70c317004bbd3fcb2f418b5
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-11-08 14:48:03 -0500

    ....

--- controllers/CountriesController.js
+++ controllers/CountriesController.js
@@ -18,12 +18,12 @@ const index = async(req,res) => {
 
 // Create New Country
 const create = async (req,res) => {
-    const {country_name, continent_id} = req.body;
+    const {continent_id, country_name, country_img} = req.body;
     console.log("before bd call");
     
     try{
-        const [result]= await db.query("INSERT INTO country (country_name, continent_id) VALUES (?,?)", [country_name, continent_id]);
-        res.json({countryId: result.insertId, country_name, continent_id})
+        const [result]= await db.query("INSERT INTO country (continent_id, country_name, country_img) VALUES (?,?)", [continent_id, country_name, country_img]);
+        res.json({countryId: result.insertId,continent_id, country_name, country_img})
     }catch(error){
         console.error("Error creating Country", error);
         res.status(500).json({message: "Database Countries connection error"});
