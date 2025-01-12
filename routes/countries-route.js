f1f8b328c773222cd4f87ec1d46d11ec24b94972
RadojkoRadulovic <radulovicradojko@gmail.com>
2025-01-12 00:16:03 -0500

    ...

--- routes/countries-route.js
+++ routes/countries-route.js
@@ -5,6 +5,5 @@ const CountriesController = require("../controllers/CountriesController");
 router.get('/', CountriesController.index);
 router.post('/', CountriesController.create);
 router.delete('/:id', CountriesController.deleteCountry);
-router.get("/blogs", CountriesController.getBlogsByCountry);
 
 module.exports = router
