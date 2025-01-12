b6205d9a2c50ba944075f2483bf1ced7ca657c40
RadojkoRadulovic <radulovicradojko@gmail.com>
2025-01-11 20:40:02 -0500

    ....

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ const path = require('path');
 //PORT
 const PORT = process.env.PORT;
 
-// Putanja do sertifikata
-const options = {
-    cert: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem'),
-    key: fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem'),
-};
-
 const app = express();
 
 app.use(cors());
@@ -19,6 +13,6 @@ app.use(express.json());
 app.use(routes)
 app.use('/images', express.static(path.join('/home/radojko/blog/public/images')));
 
-https.createServer(options, app).listen(PORT,'0.0.0.0', () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log("Listening on port 3001 ............");
 })
